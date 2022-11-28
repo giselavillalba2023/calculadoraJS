adca809401a483b841498a76963d439cc1492b2c
giselavillalba2023 <116774026+giselavillalba2023@users.noreply.github.com>
2022-11-28 16:08:13 -0300

    Add files via upload

---
+++ calculadora html js/index.js
@@ -0,0 +1,56 @@
+// https://www.youtube.com/watch?v=bmGPv687toc&t=454s
+/// imprimir algo en la consola
+console.log("hola"); //minuto 3:27
+//nombre de variable nro igual a 5
+//las valiables puede ser numero, decimales, texto y booleano
+//var nro = 5      
+// forma ideal y tecnica de nombrar una varieble es con CAMEL CASE ejemplo: miNro
+var miNro = 25     // Valor nemerico
+console.log(miNro);
+
+var miTexto = "Gaspar" // valor texto
+console.log(miTexto);
+//minuto 7:27
+
+var miBooleno = true //valor booleano true o false. Un bolleano es muy primitivo hacer referencia a 0 apagado y 1 prendido
+console.log(miBooleno);
+
+// crear mensajes concatenando variables
+var miMsj = "Mi edad es: " + miNro + "crear mensajes concatenando variables";
+console.log(miMsj);
+
+var miSuma = 2 + 3; //sumar numeros
+console.log("Mi suma es: " + miSuma);
+
+var miResta = 2 - 3; //restar numeros
+console.log("Mi resta es: " + miResta);
+
+var miDivision = 2/3; //sumar numeros
+console.log("Mi división es: " + miDivision);
+
+var miMultiplicacion = 2 * 3; //sumar numeros
+console.log("Mi multiplicación es: " + miMultiplicacion);
+
+//Operaciones aritmeticas son la suma, resta, mutiplicación y multiplicación solo con variables
+var Operador1 = 8; 
+console.log("Operador 1 es: " + Operador1);
+
+var Operador2 = 4; 
+console.log("Operador 2 es: " + Operador2);
+
+var sumarVariables = Operador1 + Operador2;
+console.log("La suma de las variables/ operadores es: " + sumarVariables);
+
+var restaVariables = Operador1 - Operador2;
+console.log("La resta de las variables/ operadores es: " + restaVariables);
+
+var divVariables = Operador1 / Operador2;
+console.log("La división de las variables/ operadores es: " + divVariables);
+
+var multiVariables = Operador1 * Operador2;
+console.log("La multiplicación de las variables/ operadores es: " + multiVariables);
+
+// minuto 16:03
+
+
+
